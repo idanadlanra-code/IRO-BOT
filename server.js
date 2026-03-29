780e809e668ed92bf36dbc1d5f0d449da85c2f7a
idanadlanra-code <idanadlan.ra@gmail.com>
2026-03-30 01:03:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.post("/webhook", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("🚀 Server running on", PORT);
 });
